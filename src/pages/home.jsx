219f483d0ae343a908670b756038582a23d18834
Kieran Lomas <kieran.lomas@bom.gov.au>
2021-01-25 10:04:11 +0000

    Create Discount Popup

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 
+import DiscountPopup from '../components/discountPopup'
+
 import './styles.scss'
 
 const Home = () => {
+  const [openDiscountModal, setOpenDiscountModal] = useState(false)
+
+  const handleOpenDiscountModal = () => {
+    setOpenDiscountModal(true)
+  };
+
+  const handleCloseDiscountModal = () => {
+    setOpenDiscountModal(false)
+  };
+
   return (
     <Grid
       className="home"
@@ -15,9 +27,14 @@ const Home = () => {
       spacing={2}
     >
       <Grid item>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpenDiscountModal}
+        >
           Set Daily Opportunities
         </Button>
+        <DiscountPopup open={openDiscountModal} handleClose={handleCloseDiscountModal} />
       </Grid>
     </Grid>
   )
